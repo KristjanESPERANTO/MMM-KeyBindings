96730e7d0d8702c50cce186d82f2e8d25613e4e2
Kristjan SCHMIDT <35647502+KristjanESPERANTO@users.noreply.github.com>
2023-08-16 02:09:41 +0200

    Fix path

--- postinstall.sh
+++ postinstall.sh
@@ -2,13 +2,13 @@
 # Title         : postinstall.sh
 # Description   : This script will create a udev symlink for a Bluetooth Remote
 # Author        : shbatm
-# Date          : 2021-01-09
-# Version       : 0.0.2
+# Date          : 2023-08-16
+# Version       : 0.0.3
 # Usage         : ./postinstall.sh
 #==============================================================================
 
 echo "Get the version of electron used by MagicMirror and rebuild native packages"
 ELECVERSION=$(node -pe "require('../../node_modules/electron/package.json').version")
-./node_modules/.bin/@electron/rebuild -f -w evdev,udev -v $ELECVERSION
+./node_modules/@electron/rebuild -f -w evdev,udev -v $ELECVERSION
 
 exit 0
